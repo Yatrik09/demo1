42b36f6ae9ff545ba5f0d7335b272492a1ed23d2
Yatrik <yatrikpatel1331@gmail.com>
2025-02-24 14:42:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import About from "./component/about";
 import Navbar from "./component/Navbar";
+import Contact from "./component/Contact";
 import{BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App(){
   return(
     <>
     <BrowserRouter>
-  
+    <Navbar />
     <Routes>
       <Route path="/about" element={<About/>} />
-      <Route path="/" element={<Navbar/>} />
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
     </BrowserRouter>
     </>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -21,38 +21,10 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="#">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/about">about</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="#">Link</Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="#">Action</Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">Another action</Link>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" /> {/* Corrected here */}
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">Something else here</Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <span className="nav-link disabled" aria-disabled="true">Disabled</span>
+                <Link className="nav-link" to="/contact">Contact</Link>
               </li>
             </ul>
             <form className="d-flex" role="search">
